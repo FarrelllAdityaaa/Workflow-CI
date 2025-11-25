0fc3322a7609c5504b509eb4b0a09bc6ea8929d7
FarrelllAdityaaa <farrelpaksiaditya@gmail.com>
2025-11-25 19:51:48 +0700

    perubahan kode.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,7 +3,6 @@ import mlflow
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 
 # Load Data
 df = pd.read_csv('titanic_preprocessing.csv')
@@ -14,15 +13,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Set experiment
 mlflow.set_experiment("CI_Retraining_Run")
 
-# Start run
-model = RandomForestClassifier(n_estimators=100)
-model.fit(X_train, y_train)
+# Autolog
+mlflow.sklearn.autolog()
 
-# Log metrik
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-mlflow.log_metric("accuracy", accuracy)
+with mlflow.start_run():
+    model = RandomForestClassifier(n_estimators=100)
+    model.fit(X_train, y_train)
+    print("Model Basic trained with Autolog.")
 
-# Log model
-mlflow.sklearn.log_model(model, "model")
-print("Model CI Training SUCCESS. Artifact logged locally.")
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model"
+    )
+    print("Model logged to MLflow.")
+
+    accuracy = model.score(X_test, y_test)
+    mlflow.log_metric("accuracy", accuracy)
