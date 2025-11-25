f6482f252e610a43b8fe3064a42f3fd0db3fd266
FarrelllAdityaaa <farrelpaksiaditya@gmail.com>
2025-11-25 19:39:05 +0700

    perubahan kode.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,6 +3,7 @@ import mlflow
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 # Load Data
 df = pd.read_csv('titanic_preprocessing.csv')
@@ -13,10 +14,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Set experiment
 mlflow.set_experiment("CI_Retraining_Run")
 
-# Autolog
-mlflow.sklearn.autolog()
+# Start run
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
 
-with mlflow.start_run(run_name="CI_Base_Model"):
-    model = RandomForestClassifier(n_estimators=100)
-    model.fit(X_train, y_train)
-    print("Model CI Basic trained with Autolog.")
+# Log metrik
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+mlflow.log_metric("accuracy", accuracy)
+
+# Log model
+mlflow.sklearn.log_model(model, "model")
+print("Model CI Training SUCCESS. Artifact logged locally.")
