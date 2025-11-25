6d520d3e2acfe689ba229e81576e12ce0f71cc1b
FarrelllAdityaaa <farrelpaksiaditya@gmail.com>
2025-11-25 20:06:20 +0700

    perubahan kode.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -10,9 +10,6 @@ X = df.drop('Survived', axis=1)
 y = df['Survived']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Set experiment
-mlflow.set_experiment("CI_Retraining_Run")
-
 
 model = RandomForestClassifier(n_estimators=100)
 model.fit(X_train, y_train)
