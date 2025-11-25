295c5b88acdff9e92a98749251b929f0a5cedc5d
FarrelllAdityaaa <farrelpaksiaditya@gmail.com>
2025-11-25 19:59:28 +0700

    perubahan kode.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -13,16 +13,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Set experiment
 mlflow.set_experiment("CI_Retraining_Run")
 
-with mlflow.start_run():
-    model = RandomForestClassifier(n_estimators=100)
-    model.fit(X_train, y_train)
-    print("Model Basic trained with Autolog.")
 
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model"
-    )
-    print("Model logged to MLflow.")
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+print("Model Basic trained with Autolog.")
 
-    accuracy = model.score(X_test, y_test)
-    mlflow.log_metric("accuracy", accuracy)
+mlflow.sklearn.log_model(
+    sk_model=model,
+    artifact_path="model"
+)
+print("Model logged to MLflow.")
+
+accuracy = model.score(X_test, y_test)
+mlflow.log_metric("accuracy", accuracy)
